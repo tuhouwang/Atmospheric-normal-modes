825b57ec1c4428690c2f34743c79e1a11880aee1
Houwang Tu <tuhouwang96@163.com>
2020-12-05 14:54:58 +0800

    增加了线图处的插值

--- src/SynthesizeSoundField.m
+++ src/SynthesizeSoundField.m
@@ -1,8 +1,9 @@
-function tl = SynthesizeSoundField(r,kr,psizs,psi)
+function [tl, tl_zr] = SynthesizeSoundField(r,kr,z,zr,psizs,psi)
 
     range = kr * r;
     range = exp(1i * range) ./ sqrt(range);
     p     = psi * diag(psizs) * range * sqrt(2 * pi);
     tl    = -20 * log10(abs(p));
+    tl_zr = interp1(z,tl,zr,'linear');
 
 end
